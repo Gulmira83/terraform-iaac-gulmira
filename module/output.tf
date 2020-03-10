086fdba3e2d6cc8626a562c3da1b4925494de090
Cloud User <centos@ip-172-31-18-178.eu-west-2.compute.internal>
2020-03-10 19:33:48 +0000

    add

---
+++ module/output.tf
@@ -0,0 +1,22 @@
+output "IP" {
+  value = "${aws_instance.web.public_ip}"
+}
+output "KEY" {
+  value = "${aws_instance.web.key_name}"
+}
+
+output "Bucketname" {
+  value = "${aws_s3_bucket.b2.bucket}"
+}
+
+output "SEC_GROUP" {
+  value = "${aws_security_group_tls.name}"
+}
+
+output "AMI" {
+  value = "${aws_instance.web.ami}"
+}
+
+output "ROUTE53" {
+  value = "${aws_route53.wordpress.name}"
+}
