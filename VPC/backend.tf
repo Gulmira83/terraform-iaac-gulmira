e742924f89fb3c80ef57013d3efa9b1c940d5bd2
Cloud User <centos@ip-172-31-18-178.eu-west-2.compute.internal>
2020-03-11 01:31:53 +0000

    add

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "terraform-class-gulmira"
-    key    = "path/to/my/vpc"
+    key    = "path/to/my/us-east-1"
     region = "us-east-1"
-  # dynamodb_table = "terraform-class"
+    #dynamodb_table = "terraform-class20"
   }
 }
