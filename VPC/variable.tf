535c320574c21104e90f3804fcd774435bc468ed
Cloud User <centos@ip-172-31-18-178.eu-west-2.compute.internal>
2020-03-11 23:08:39 +0000

    added

--- VPC/variable.tf
+++ VPC/variable.tf
@@ -1,16 +1,11 @@
-variable "region" {}
+variable "region" {} 
 
 variable "cidr_block" {}
-
 
 variable "cidr_block1_public" {}
 variable "cidr_block2_public" {}
 variable "cidr_block3_public" {}
 
-
 variable "cidr_block1_private" {}
 variable "cidr_block2_private" {}
 variable "cidr_block3_private" {}
-
-
-
