ed145cfab0d755afb2628a8a9a76e45b3ce834d3
Cloud User <centos@ip-172-31-18-178.eu-west-2.compute.internal>
2020-03-11 00:03:31 +0000

    add

--- VPC/rt.tf
+++ VPC/rt.tf
@@ -15,5 +15,26 @@ resource "aws_route_table_association" "dev" {
   route_table_id = "${aws_route_table.dev.id}" 
 
 } 
+resource "aws_route_table_association" "dev1" { 
 
+  subnet_id = "${aws_subnet.dev1.id}" 
+
+  route_table_id = "${aws_route_table.dev.id}" 
+}
+
+
+resource "aws_route_table_association" "dev2" { 
+
+  subnet_id = "${aws_subnet.dev1.id}" 
+
+  route_table_id = "${aws_route_table.dev.id}" 
+}
+
+
+resource "aws_route_table_association" "dev3" { 
+
+  subnet_id = "${aws_subnet.dev1.id}" 
+
+  route_table_id = "${aws_route_table.dev.id}" 
+}
   
