35543792bcb222328922adadda5135bc3585c755
Cloud User <centos@ip-172-31-18-178.eu-west-2.compute.internal>
2020-03-11 23:07:12 +0000

    added

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "terraform-class-gulmira"
-key = "tower/us-east-1/tools/Oregon/tower.tfstate"
+key = "tower/us-east-1/tools/Virgina/tower.tfstate"
 region = "us-east-1"
   }
 }
