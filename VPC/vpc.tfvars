e9bec83a1f819ec344996e77a60c819e33c3d38d
Cloud User <centos@ip-172-31-18-178.eu-west-2.compute.internal>
2020-03-11 00:59:55 +0000

    add

--- VPC/vpc.tfvars
+++ VPC/vpc.tfvars
@@ -1,4 +1,4 @@
-region = "eu-west-2"
+region = "us-east-3"
 
 cidr_block = "10.0.0.0/16"
 
--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "terraform-class-gulmira"
-    key    = "path/to/my/key"
-    region = "us-east-2"
+    key    = "path/to/my/us-east-2"
+    region = "us-east-1"
   # dynamodb_table = "terraform-class"
   }
 }
